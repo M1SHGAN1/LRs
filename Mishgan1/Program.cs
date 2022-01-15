5a861789f66f32d0c3d6faef2ddc34d92a8b207c
user <mparinos56@gmail.com>
2022-01-15 11:15:20 +0300

    Добавьте файлы проекта.

---
+++ Mishgan1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Mishgan1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MallardDuck duck1 = new MallardDuck();
+            RedHeadDuck duck2 = new RedHeadDuck();
+
+            Duck[] stringArray = new Duck[] { duck1, duck2 };
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                Console.WriteLine(stringArray[i].Display());
+                Console.WriteLine(stringArray[i].Swim());
+                Console.WriteLine(stringArray[i].Quack());
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+
+    abstract class Duck
+    {
+        public string Quack()
+        {
+            return "Quack" + this.GetType();
+        }
+
+        public string Swim()
+        {
+            return "Swim" + this.GetType();
+        }
+        public abstract string Display();
+
+    }
+
+    class MallardDuck : Duck
+    {
+        public override string Display()
+        {
+            return "Display" + this.GetType();
+        }
+    }
+
+    class RedHeadDuck : Duck
+    {
+        public override string Display()
+        {
+            return "Display" + this.GetType();
+        }
+
+    }
+}
